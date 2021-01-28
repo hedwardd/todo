cb5a760c5f4a0762f3487a754f542fad5eab9aa2
Heath Daniel <mr.heath.daniel@gmail.com>
2021-01-28 15:18:52 -0500

    Tidy code formatting

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const db = require("./app/models");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
-
-
-const db = require("./app/models");
 
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
@@ -19,6 +16,7 @@ db.sequelize.sync({ force: true }).then(() => {
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+// Set routes
 require("./app/routes/task.routes")(app);
 
 // The 'catchall' handler: for any request that doesn't
@@ -31,13 +29,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-
-// (async () => {
-//   const db = await initDb();
-//   if (db) {
-//     const port = process.env.PORT || 5000;
-//     app.listen(port);
-//     console.log(`App listening on ${port}`);
-//   }
-// })();
