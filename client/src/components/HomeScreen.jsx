f2249f60fd7b00d234b8c7391905363992d23192
Heath Daniel <mr.heath.daniel@gmail.com>
2021-02-18 20:58:06 -0500

    Start list creation form

--- client/src/components/HomeScreen.jsx
+++ client/src/components/HomeScreen.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const HomeScreen = (props) => {
 
+  const [isCreating, setIsCreating] = useState(null);
+  const [aliasInput, setAliasInput] = useState("");
+
+  const headerText = isCreating
+    ? "Pick an alias..."
+    : "Enter your alias...";
+
+  const buttonText = isCreating
+    ? "Create"
+    : "Go to List";
+
   return (
     <div>
-      <p>Hello World!</p>
+      {isCreating == null ? (
+        <div>
+          <div>
+            <h2>Create New List</h2>
+            <button
+              onClick={() => setIsCreating(true)}
+            >Create</button>
+          </div>
+          
+          <div>
+            <h2>Use Existing</h2>
+            <button
+              onClick={() => setIsCreating(false)}
+            >Search</button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <button
+            onClick={() => setIsCreating(null)}
+          >
+            Back
+          </button>
+
+          <h2>
+            {headerText}
+          </h2>
+
+          <input
+            placeholder="my-list"
+          />
+
+          <button disabled>
+            {buttonText}
+          </button>
+        </div>
+      )}
     </div>
   );
 }
