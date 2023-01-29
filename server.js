350fc0241b5cea9bbb2d2511b2b0fbe4434eda02
Heath Daniel <mr.heath.daniel@gmail.com>
2023-01-28 19:44:22 -0500

    Change server port

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
